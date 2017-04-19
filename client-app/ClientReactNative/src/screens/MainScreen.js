a99995511e1e74c090f3d633f9acb2b00cb9e7d2
Son Trinh <xuanson.trinh@outlook.com>
2017-04-20 00:36:29 +0700

    Init ListDish Fragment

--- client-app/ClientReactNative/src/screens/MainScreen.js
+++ client-app/ClientReactNative/src/screens/MainScreen.js
@@ -7,24 +7,49 @@ import {
 } from 'react-native';
 
 import * as firebase from 'firebase';
+import { Container, Content, Tab, Tabs, Header, Body, Title } from 'native-base';
+import { Actions } from 'react-native-router-flux';
+import ListDish from './MainScreenFragments/ListDish';
 
 class MainScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            currentUser: null
         }
     }
 
-    render(){
-        return (
-        <View>
-            <Text>
-                ahihi
-            </Text>
-        </View>)
+    componentWillMount() {
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ currentUser: user });
+
+            } else {
+                Actions.pop();
+            }
+        });
     }
-        
+
+    render() {
+        return (
+            <Container>
+                <Header hasTabs>
+                    <Body>
+                        <Title>Main Menu</Title>
+                    </Body>
+                </Header>
+                <Tabs>
+                    <Tab heading="List Order">
+                        <ListDish />
+                    </Tab>
+                    <Tab heading="Notification">
+                        <View />
+                    </Tab>
+                </Tabs>
+            </Container>
+        );
+    }
+
 }
 
 module.exports = MainScreen;
